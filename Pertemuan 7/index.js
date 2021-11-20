4d147432d461d55a4f75c8575cc7abe52446a160
feronicaw <feronicawie@gmail.com>
2021-11-20 20:07:18 +0700

    Pertemuan 7 - Akhir

--- Pertemuan 7/index.js
+++ Pertemuan 7/index.js
@@ -1,18 +1,51 @@
-//latihan 7.2
+//latihan 7.8
 var http = require('http');
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
-http.createServer((req, res)=>{
-  if (req.url=='/home')
-    res.write('<p>Konten Home</p>')
-  else if (req.url=='/product')
-    res.write('<p>Konten Product</p>')
-  else if (req.url=='/order')
-    res.write('<p>Konten Order</p>')
-  else if (req.url=='/')
-    res.write('<p>Pengembangan Aplikasi Web dengan Nodejs</p>')
-  else
-    res.write('<h1>404</h1>')
-  res.end();
-}).listen(8000, ()=>{
-  console.log('Server is running on port 8000')
-});
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+var persons =[]
+
+app.post('/person',(req, res)=>{
+    persons.push(req,body)
+    res.json(req,body)
+})
+
+app.get('/person',(req, res)=>{
+    
+    res.json(persons)
+})
+http.createServer(app)
+.listen(8000,()=>{
+    console.log('Server is running on port 8000')
+})
+// //latihan 7.3-7.7
+// var express = require('express')
+// var app = express()
+
+// app.get('/users/:userID/books/:bookID', (req, res)=>{
+//   res.end(req.params)
+// })
+// app.listen(8000, ()=>{console.log('Server is running on port 8000')})
+
+//latihan 7.2
+// var http = require('http');
+
+// http.createServer((req, res)=>{
+//   if (req.url=='/home')
+//     res.write('<p>Konten Home</p>')
+//   else if (req.url=='/product')
+//     res.write('<p>Konten Product</p>')
+//   else if (req.url=='/order')
+//     res.write('<p>Konten Order</p>')
+//   else if (req.url=='/')
+//     res.write('<p>Pengembangan Aplikasi Web dengan Nodejs</p>')
+//   else
+//     res.write('<h1>404</h1>')
+//   res.end();
+// }).listen(8000, ()=>{
+//   console.log('Server is running on port 8000')
+// });
